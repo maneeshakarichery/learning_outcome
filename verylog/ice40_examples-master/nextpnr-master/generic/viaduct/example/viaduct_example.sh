07913b0d15f4ffb1a8113a0fcd2cb4f5bff2c05f
maneeshakarichery <160504647+maneeshakarichery@users.noreply.github.com>
2024-04-13 21:10:00 +0530

    Delete verylog directory

--- verylog/ice40_examples-master/nextpnr-master/generic/viaduct/example/viaduct_example.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-# Run synthesis
-yosys -p "tcl synth_viaduct_example.tcl blinky.json" ../../examples/blinky.v
-# Run PnR
-${NEXTPNR:-../../../build/nextpnr-generic} --uarch example --json blinky.json
